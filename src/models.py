eddc1344334d6b25e579a5e63e695006488b472c
koinangestreets11 <koinangestreets11@gmail.com>
2025-09-09 19:56:53 +0300

    fixed laggin

--- src/models.py
+++ src/models.py
@@ -2,7 +2,9 @@ from typing import Literal
 import timm
 
 
-def build_model(num_classes: int = 4, backbone: str = "efficientnet_b0", pretrained: bool = True):
-	"""Create a classification model using timm backbones."""
+def build_model(num_classes: int = 4, backbone: str = "efficientnet_b0", pretrained: bool = False):
+	"""Create a classification model using timm backbones.
+	Note: default pretrained=False to avoid network downloads in container runtime.
+	"""
 	model = timm.create_model(backbone, pretrained=pretrained, num_classes=num_classes)
 	return model
